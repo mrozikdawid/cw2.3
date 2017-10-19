ddc546eb1c6ab4bc68fe216145eab546ef373f06
mrozikdawid <mrozik.dawid@gmail.com>
2017-10-19 14:29:34 +0200

    Add files via upload

---
+++ cw2.3/cw2.3/Pacjent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw2._3
+{
+    class Pacjent
+    {
+        public string imie;
+        public string nazwisko;
+        public double wzrost;
+        public double waga;
+
+
+        public void ObliczBmi()
+        {
+            Console.WriteLine((waga)/(wzrost*wzrost));
+        }
+
+    }
+}
---
+++ cw2.3/cw2.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pacjent pacjent1 = new Pacjent();
+            pacjent1.imie = "Jacek";
+            pacjent1.nazwisko = "Prociak";
+            pacjent1.wzrost = 1.76;
+            pacjent1.waga = 105;
+
+            pacjent1.ObliczBmi();
+
+            Console.ReadKey();
+
+        }
+    }
+}
